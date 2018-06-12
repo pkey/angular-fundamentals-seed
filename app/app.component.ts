53567182a940f603cf264a7b920c1ceaff071ff5
Paulius Kutka <kutka100@gmail.com>
2018-06-12 16:06:13 +0300

    Finished 'Pipes for date transformation'

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from "@angular/core";
 
 //Defining custom interfaces, types
 interface Passenger {
-  id: number;
-  fullName: string;
-  checkedIn: boolean;
+  id: number,
+  fullName: string,
+  checkedIn: boolean,
+  //Question marke to indicate that the value is optional
+  checkInDate?: number
 }
 @Component({
   selector: "app-root",
@@ -12,7 +14,6 @@ interface Passenger {
     template: `
         <div class="app">
             <h3>Airline Passengers</h3>
-            <!--> Many way to do it. First: <--->
         <ul>
             <li *ngFor="let passenger of passengers; let i = index;">
             <span 
@@ -20,46 +21,15 @@ interface Passenger {
             [class.checked-in]="passenger.checkedIn"
             ></span>
                {{ i }}: {{passenger.fullName}}
+               <!---> Json pipe - cool <-->
+               <p> {{ passenger | json }}  </p>
+               <div class="date">
+                  <!---> Date pipe - can specify different date formats <-->
+                  <!---> And pipes can be chained <-->
+                Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }} 
+               </div>
             </li>
         </ul>
-         <!--> Second: <--->
-         <h3>Airline Passengers</h3>
-        <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-        <span 
-        class="status"
-        [ngClass]= "{ 
-            'checked-in': passenger.checkedIn,
-            'checked-out': !passenger.checkedIn
-        }"
-    ></span>
-           {{ i }}: {{passenger.fullName}}
-        </li>
-    </ul>
-    <!--> Styles now. First: <--->
-    <h3>Airline Passengers</h3>
-    <ul>
-    <li *ngFor="let passenger of passengers; let i = index;">
-    <span 
-    class="status"
-    [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"
-></span>
-       {{ i }}: {{passenger.fullName}}
-    </li>
-</ul>
- <!--> Styles now. Second: <--->
-<h3>Airline Passengers</h3>
-    <ul>
-    <li *ngFor="let passenger of passengers; let i = index;">
-    <span 
-    class="status"
-    [ngStyle]="{
-      backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
-    }"
-></span>
-       {{ i }}: {{passenger.fullName}}
-    </li>
-</ul>
         </div>
     `
 })
@@ -68,22 +38,26 @@ export class AppComponent {
     {
       id: 1,
       fullName: "Stephen",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000
     },
     {
       id: 2,
       fullName: "Rose",
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     },
     {
       id: 3,
       fullName: "James",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1491606000000
     },
     {
       id: 4,
       fullName: "Louise",
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000
     },
     {
       id: 5,
