1db7783bd8e51dc5d923c60891c26e33970ffab0
Paulius Kutka <kutka100@gmail.com>
2018-06-12 16:50:19 +0300

    Finished 'Safe navigation operator'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from "@angular/core";
 
 //Defining custom interfaces, types
+interface Child {
+  name: string,
+  age: number
+}
 interface Passenger {
   id: number,
   fullName: string,
   checkedIn: boolean,
   //Question marke to indicate that the value is optional
-  checkInDate?: number
+  checkInDate?: number,
+  //'| null' to indicate that it should be defined but can be null.
+  //Used hwne we asssume that APi wil send back the property
+ // checkInDate: number | null
+ children: Child[] | null
 }
 @Component({
   selector: "app-root",
@@ -28,6 +36,10 @@ interface Passenger {
                   <!---> And pipes can be chained <-->
                 Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }} 
                </div>
+               <div class="children">
+                <!---> Safe navigation operator - ignores if null. Or operator may be displayed if the element is not defined. <-->
+                  Children: {{ passenger.children?.length || 0 }}
+               </div>
             </li>
         </ul>
         </div>
@@ -39,30 +51,36 @@ export class AppComponent {
       id: 1,
       fullName: "Stephen",
       checkedIn: true,
-      checkInDate: 1490742000000
+      checkInDate: 1490742000000,
+      children: []
     },
     {
       id: 2,
       fullName: "Rose",
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: null
     },
     {
       id: 3,
       fullName: "James",
       checkedIn: true,
-      checkInDate: 1491606000000
+      checkInDate: 1491606000000,
+      children: []
     },
     {
       id: 4,
       fullName: "Louise",
       checkedIn: true,
-      checkInDate: 1490742000000
+      checkInDate: 1490742000000,
+      children: null
     },
     {
       id: 5,
       fullName: "Tina",
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null,
+      children: []
     }
   ];
 }
